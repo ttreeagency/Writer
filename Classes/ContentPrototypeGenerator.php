4fbd48048de7ad72527976ac77cea5d8b558fa66
Dominique Feyer <dfeyer@ttree.ch>
2018-05-23 13:38:24 +0200

    TASK: Use Carbon.Notification to show Prototype generator error

---
+++ Classes/ContentPrototypeGenerator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Ttree\Writer;
+
+use Neos\ContentRepository\Domain\Model\NodeType;
+use Neos\Flow\Annotations as Flow;
+use Neos\Neos\Domain\Service\DefaultPrototypeGeneratorInterface;
+
+/**
+ * @Flow\Scope("singleton")
+ * @api
+ */
+class ContentPrototypeGenerator implements DefaultPrototypeGeneratorInterface
+{
+    /**
+     * @var string
+     */
+    protected $basePrototypeName = 'Neos.Neos:ContentComponent';
+
+    public function generate(NodeType $nodeType)
+    {
+        if (strpos($nodeType->getName(), ':') === false) {
+            return '';
+        }
+
+        $prototypeName = $nodeType->getName() . '.Writer';
+
+        $output = [];
+        $output[] = 'prototype(' . $prototypeName . ') < prototype(Carbon.Notification:Backend) {';
+        $output[] = '    type = \'alert\'' . chr(10);
+        $output[] = '    content = \'Configure the prototype <b>' . $prototypeName . '</b>\'';
+        $output[] = '}';
+
+        return implode(chr(10), $output);
+    }
+}
